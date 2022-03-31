cd8cbd00961a4e6fd71af1f6de7cce3b48b3da09
Rayhan <rayhanprima99@gmail.com>
2022-03-31 19:40:02 +0700

    chore: fix bug on text & main component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "../App.css";
 import Form from "../components/Form";
 import Table from "../components/Table";
